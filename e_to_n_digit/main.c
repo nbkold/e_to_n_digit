bed4704dcb9071a76f547c02a39d4ea53578058e
nbkold <kickjerry@gmail.com>
2017-01-22 23:17:31 -0800

    Initial Commit

---
+++ e_to_n_digit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  e_to_n_digit
+//
+//  Created by Weiye Wu on 1/22/17.
+//  Copyright © 2017 Weiye Wu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
